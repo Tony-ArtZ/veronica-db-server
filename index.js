8e9dc5d20ad77c2b26fae280aec3b6543d9232cd
BT08249 <adarshgku@gmail.com>
2023-09-23 00:02:15 +0530

    added route and model for auth tokens

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from "express";
 import "dotenv/config";
 import mongoose from "mongoose";
 import { todoRouter } from "./routes/todo.js";
+import { authStorageRouter } from "./routes/auth-storage.js";
 
 const app = express();
 
 app.use(express.json());
 app.use("/todo", todoRouter);
+app.use("/storetoken", authStorageRouter);
 
 mongoose.connect(process.env.MONGO_DB);
 
