91f459c3c21eacfdb6769afdee9d4bfd772a3049
BT08249 <adarshgku@gmail.com>
2023-09-27 20:16:28 +0530

    added server ping route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ import { authStorageRouter } from "./routes/auth-storage.js";
 
 const app = express();
 
+app.get("/ping", (req, res) => {
+    res.send("Pong!");
+})
+
 app.use(express.json());
 app.use("/todo", todoRouter);
 app.use("/storetoken", authStorageRouter);
